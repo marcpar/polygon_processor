bdf90705ca7f870f01dec2b38399a48a4411b166
Marc Par <marc.par10@gmail.com>
2023-03-21 16:11:43 +0800

    claimable page layout

---
+++ frontend/claimables/src/components/media/Media.tsx
@@ -0,0 +1,73 @@
+import styles from "./Media.module.css";
+import { CircleLoader, BounceLoader } from "react-spinners";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { detect } from "detect-browser";
+
+type MediaProps = {
+    src: string
+    isLoadingSetter?: Dispatch<SetStateAction<boolean>>
+}
+
+
+async function getMediaSafari(src: string): Promise<string> {
+    let result = await fetch(`${src}`, {
+        redirect: 'follow'
+    });
+    let blob = await result.arrayBuffer();
+    let buffer = Buffer.from(blob);
+    let b64 = buffer.toString('base64');
+    let contentType = result.headers.get('Content-Type');
+    return `data:${contentType ?? ""};base64,${b64}`
+    
+}
+
+export default function Media(props: MediaProps) {
+    let browser = detect();
+    let ext = props.src.split(".").pop();
+    let [isLoading, setIsLoading] = useState<boolean>(true);
+    let [mediaSrc, setMediaSrc] = useState<string>('');
+
+    function onLoadHandler() {
+        setIsLoading(false);
+        if (props.isLoadingSetter !== undefined && props.isLoadingSetter !== null) {
+            props.isLoadingSetter(false);
+        }
+    }
+
+    useEffect(() => {
+        if (mediaSrc !== '') {
+            return;
+        }
+        if (browser && (browser.name === 'safari' || browser.name === 'crios' || browser.name === 'ios' || browser.name === 'ios-webview')) {
+            getMediaSafari(props.src).then((src) => {
+                setIsLoading(false);
+                setMediaSrc(src);
+            });
+        } else {
+            setMediaSrc(props.src);
+        }
+    }, [mediaSrc, browser, props.src]);
+    
+    
+    switch (ext) {
+        case "mp4" || "webp":
+            return (
+                <div className={styles.media_container}>
+                    <video src={mediaSrc} autoPlay muted playsInline={true} controls={false} loop className={isLoading ? styles.hidden : styles.media} onPlay={() => {onLoadHandler()}}/>
+                    <BounceLoader className={styles.loader} loading={isLoading} color={"rgb(0, 98, 190)"} />
+                </div>
+            );
+        case undefined:
+            return (
+                <CircleLoader />
+            );
+        default:
+            return (
+                <div className={styles.media_container}>
+                    <img src={mediaSrc} className={isLoading ? styles.hidden : styles.media} onLoad={() => {onLoadHandler()}} alt={"nft"} />
+                    <BounceLoader className={styles.loader} loading={isLoading} color={"rgb(0, 98, 190)"} />
+                </div>
+            );
+    }
+
+}
--- frontend/claimables/src/pages/claim/[contractAddress]/[tokenID].tsx
+++ frontend/claimables/src/pages/claim/[contractAddress]/[tokenID].tsx
@@ -1,11 +1,90 @@
 import { useRouter } from 'next/router';
+import style from '@/styles/claim.module.css';
+import Tilt from 'react-parallax-tilt';
+import Podium from '@/assets/PODIUM-VirtualMedal.png';
+import ordinal from 'ordinal';
+import { useState } from 'react';
+import Media from '@/components/media/Media';
+import { BrowserProvider, Provider } from 'ethers';
+
 
 export default function ClaimNFT() {
     const router = useRouter();
     let contractAddress = router.query.contractAddress as string;
     let tokenID = router.query.tokenID as string;
+    let [isClaimable, setIsClaimable] = useState<boolean>(true);
+    let [isAlreadyClaimed, setIsAlreadyClaimed] = useState<boolean>(false);
+    let [isMediaLoading, setIsMediaLoading] = useState<boolean>(true);
+
+    function claimOnClick() {
+        alert(`${contractAddress}/${tokenID}`);
+    }
+
+    function onClickDownload() {
+        let eth = (window as any).ethereum;
+        if (eth) {
+            let metamask = new BrowserProvider(eth);
+            metamask.getSigner().then(async (signer) => {
+                let msg = await signer.signMessage('hello');
+                alert(msg);
+            })
+
+        }
+
+    }
+
+    let fullName = 'Participant';
+    let racePosition = '1';
+    let groupName = 'raceGroup';
+    let eventName = 'eventName';
+    let eventDate = 'Event Date';
 
     return (
-        <div><h1>Claim your nft {contractAddress}/{tokenID}</h1></div>
+        <div className={style.main_container}>
+            <img alt="podium_logo" className={style.podium_logo} src={Podium.src} />
+            <div className={style.greetings}>
+                <p><b>Congratulations {fullName} for coming {ordinal(parseInt(racePosition, 10))} in {groupName} at the {eventName}, {eventDate}.</b></p>
+                <p>Your virtual medal is ready to claim as an NFT, featuring many benefits. Or simply download the media file as a digital collectible.</p>
+            </div>
+            <div className={style.flex_container}>
+                <Tilt tiltReverse={true} tiltMaxAngleX={7} tiltMaxAngleY={7} glareReverse={true} >
+                    <div className={style.media_container}>
+                        <Media src={`https://arweave.net/TRTM67VNFsUzggHpVrlgyl-StaLUvC4p0lggBFj323Y/nft.mp4`} isLoadingSetter={setIsMediaLoading} />
+                    </div>
+                </Tilt>
+                <div className={style.card}>
+                    <div className={style.card_header}>NFT benefits</div>
+                    <div className={style.card_body}>
+                        <hr />
+                        <span>Digital collectible</span>
+                        <hr />
+                        <span>Social media ready</span>
+                        <hr />
+                        <span>Sponsor rewards enabled</span>
+                        <hr />
+                        <span>Blockchain certified carbon offset</span>
+                        <hr />
+                        <span>NFT minted to blockchain</span>
+                        <hr />
+                    </div>
+                    <div className={style.card_footer}>
+                        <button onClick={claimOnClick} disabled={isMediaLoading || !isClaimable || isAlreadyClaimed}>{isAlreadyClaimed ? 'Already Claimed' : 'Claim your NFT'}</button>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <div className={style.card_header}>Media file benefits</div>
+                    <div className={style.card_body}>
+                        <hr />
+                        <span>Digital collectible</span>
+                        <hr />
+                        <span>Social media ready</span>
+                        <hr />
+                    </div>
+                    <div className={style.card_footer}>
+                        <button onClick={onClickDownload}>Download</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
