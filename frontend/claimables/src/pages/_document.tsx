a90b210cef2faeb110fa0aa30451bbf3b8bb0496
Marc Par <marc.par10@gmail.com>
2023-05-22 19:09:53 +0800

    hardcode og tags

--- frontend/claimables/src/pages/_document.tsx
+++ frontend/claimables/src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Podium Labs</title>
+        <meta property="og:title" content="Podium Labs"/>
+        <meta property="og:description" content="2022 TTO Duathlon National Championships"/>
+        <meta property="og:url" content="https://bzjgxpwh7z4sqwoln7e6ibr3u3ixh7hyl673u7dlthslsum7fzuq.arweave.net/DlJrvsf-eShZy2_J5AY7ptFz_Phfv7p8a5nkuVGfLmk/nft.mp4"/>
+        <meta property="og:type" content="video.movie"/>
+        <meta property="og:image" content="https://bs7phj5tnh7xddcnn3kklvp66cy2zjzugk5bsbkwpyxcflsye5nq.arweave.net/DL7zp7Np_3GMTW7UpdX-8LGspzQyuhkFVn4uIq5YJ1s/.gif"/>
       </Head>
       <body>
         <Main />
